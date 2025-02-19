03b1780df1f153e34e7105b2f5c1736d3ebde350
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-02-19 21:20:58 +0900

    putsメゾットの出力例

---
+++ 01_puts/hello_word.rb
@@ -0,0 +1,9 @@
+puts 'Hello word'
+print 'Hello word'
+p 'Hello word'
+
+#解説
+#putsメソットは引数を文字列として出力し、最後に改行を追加。
+#printメソットは、引数を文字列として出力するが、改行は追加されない。
+#pメソッドは引数をそのまま出力する。
+#返り値はputs,printメソッドはnil、pメソッドは引数そのものになる。
---
+++ 01_puts/string_connection.rb
@@ -0,0 +1,6 @@
+puts 'Hello'
+puts 'Hello' + 'Red'
+puts 'Hello' + ' ' + 'Red'
+
+#解説
+#シングルクオートに半角のスペースを入れないと空白にならないので注意。
